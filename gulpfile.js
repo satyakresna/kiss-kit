f24d6dc5fdbb3d4d7d4c81a43857e89e73f1f1b2
Satya Kresna <satyakresna6295@gmail.com>
2019-11-20 22:54:06 +0800

    Setup gulp

---
+++ gulpfile.js
@@ -0,0 +1,83 @@
+const gulp = require('gulp');
+const rename = require('gulp-rename');
+const browserSync = require('browser-sync').create();
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const purgecss = require('gulp-purgecss');
+
+/**
+ * Custom PurgeCSS Extractor
+ * https://github.com/FullHuman/purgecss
+ */
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-z0-9-:\/]+/g);
+  }
+}
+
+gulp.task('css', async function() {
+  const postcss = require('gulp-postcss');
+  const plugins = [
+    tailwindcss('./tailwind.config.js'),
+    autoprefixer,
+    cssnano({
+      preset: 'default'
+    }),
+  ]
+  gulp.src('src/css/style.css')
+    .pipe(postcss(plugins))
+    .pipe(purgecss({
+      // Specify the paths to all of the template files in your project
+      content: [
+        './src/**/*.html',
+        './src/*.html'
+      ],
+
+      // Include any special characters you're using in this regular expression
+      extractors: [
+        {
+          extractor: TailwindExtractor,
+          extensions: ["html"]
+        }
+      ]
+    }))
+    .pipe(rename('css/style.min.css'))
+    .pipe(gulp.dest('./dist/'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+gulp.task('html', async function () {
+  gulp.src(['./src/**/.*html', './src/*.html'])
+    .pipe(gulp.dest('dist'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+gulp.task('assets', async function () {
+  gulp.src('./src/assets/**')
+    .pipe(gulp.dest('dist/assets/'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+gulp.task('watch', async function () {
+  gulp.watch('./src/css/style.css', gulp.series('css'));
+  gulp.watch(['./src/**/*.html'], gulp.series('html'));
+});
+
+gulp.task('browserSync', function () {
+  browserSync.init({
+    server: './dist'
+  })
+});
+
+// For production
+gulp.task('build', gulp.series('html', 'css', 'assets'));
+
+// For development
+gulp.task('default', gulp.series('build', 'watch', 'browserSync'));
