9d180933d9869c6cdfc3fa79303cdd1a45c49fea
Satya Kresna <satyakresna6295@gmail.com>
2020-04-19 17:05:10 +0800

    feat: track url using google analytics

--- gulpfile.js
+++ gulpfile.js
@@ -64,8 +64,25 @@ gulp.task('browserSync', function () {
   });
 });
 
-gulp.task('readEnv', async function () {
-  console.log(process.env.GOOBER_VALUE);
+gulp.task('inject:analytics', async function () {
+  const GA_TRACKER_ID = process.env.GA_TRACKER_ID;
+  gulp.src('src/index.html')
+    .pipe(inject.before('</head>',  `
+    <link rel="preconnect" href="https://www.google-analytics.com">
+    <!-- Google Analytics -->
+    <script defer>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.defer=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+      
+      ga('create', ${GA_TRACKER_ID}, 'auto');
+      ga('send', 'pageview');
+    </script>
+    `))
+    .pipe(rename('index.html'))
+    .pipe(gulp.dest('dist'));
+    done();
 });
 
 // For production
