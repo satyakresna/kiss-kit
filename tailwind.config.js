d782129cfaf9233d0d68e67cc7660a774b21ae5b
Kresna <satyakresna6295@gmail.com>
2020-08-31 09:50:04 +0800

    chore: update tailwind version and config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  purge: {
+    mode: 'all',
+    content: [
+      './src/**/*.html',
+      './src/*.html',
+      './src/js/**/*.js',
+    ]
+  },
   theme: {
     extend: {}
   },
