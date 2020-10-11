138672b0b0796966546aefd8876cb81370725c5f
Kresna <satyakresna6295@gmail.com>
2020-10-11 18:54:46 +0800

    create lite version

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,1 @@
-import page from "page";
-import Home from "./home/index.js";
-import Users from "./users/index.js";
-import NotFound from "./notfound.js";
-
-page('/', Home);
-page('/users', Users);
-page('/users/:username', Users);
-page('*', NotFound);
-// Call it!
-page();
+console.log('Hi!');
--- src/js/notfound.js
+++
@@ -1,8 +0,0 @@
-import setActiveMenu from "./utils/setActiveMenu.js";
-import setTitle from "./utils/setTitle.js";
-
-export default function (ctx) {
-  setActiveMenu(ctx.path);
-  setTitle(ctx, 'Not found');
-  document.querySelector('main').textContent = 'Not found';
-}
